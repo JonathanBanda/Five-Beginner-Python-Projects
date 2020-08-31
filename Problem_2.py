63b3b9f2c209f6eb1a95853d60bf99594903e866
Jonathan Banda <jonathanbanda00@gmail.com>
2020-08-31 02:47:47 -0500

    Finished up the small program

--- Problem_2.py
+++ Problem_2.py
@@ -10,12 +10,40 @@ Learning outcomes
 
 """
 
+# we will use this function to generate a random number
 
 from random import randint
 
+strikeCount = 0
+
+keepGoing = int(input('Hello all and welcome to the random guessing game (0-50) \n'
+	  'To begin enter 1 or any other key to exit! '))
+
+hiddenNum = randint(0,50)
+
+while keepGoing == 1:
+
+	guess = input('Please enter a number between 0-50: ')
+
+	if guess == hiddenNum:
+
+		print('you got it right!!!!')
+		keepGoing = False
+
+	else:
+
+		strikeCount += 1
+
+		print('Try again sorry')
+		print('This is your current strike ', strikeCount)
+
+		if strikeCount == 3:
+			keepGoing = False
+			print('The correct number was', hiddenNum)
+	
+
+print('Thanks Bye!!')
 
 
-print('Hello all and welcome to the random guessing game \n'
-	  'To begin enter 1 or any other key to exit!')
 
 
